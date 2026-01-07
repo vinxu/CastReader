057fa69fb366497832fdfbe1afc70afcd1dde415
vinxu <vinxu.cn@gmail.com>
2026-01-07 21:20:16 +0800

    Initial Commit

---
+++ CastReader/CastReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastReaderApp.swift
+//  CastReader
+//
+//  Created by 许旭恒 on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CastReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CastReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CastReader
+//
+//  Created by 许旭恒 on 1/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
